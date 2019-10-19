c8c3662b4fe4b4cd68a35c6e031c16692c268781
Song Jun(SZ) <cnsongju@cn.eu.sew>
2019-10-19 15:36:04 +0800

    python note

---
+++ 笨办法学python 代码笔记.py
@@ -0,0 +1,108 @@
+# name = 'Zed A. Shaw'
+# age = 35  # not a lie
+# height = 74  # inch
+# weight = 180  # lbs
+# eyes = 'Blue'
+# teeth = 'White'
+# hair = 'Brown'
+#
+#
+# print('Let\'s talk about %s.' % name)
+# print('He\'s %.2f cm tall.' % (height * 2.54))
+# print('He\'s %d pounds heavy.' % weight)
+# print('Actually that\'s not too heavy.')
+# print('He\'s got %s eyes and %s hair.' % (eyes, hair))
+# print('His teeth are usually %s depending on the coffee.' % teeth)
+# print('If I add %d, %d, and %d I get %d.' % (age, height, weight, age + height + weight))
+#
+#
+# x = 'There are %d types of people.' % 10
+# binary = 'binary'
+# do_not = 'don\'t'
+# y = 'Those who know %s and those who %s.' % (binary, do_not)
+#
+# print(x)
+# print(y)
+#
+# print('I said')
+# print('I also said:\' %s\'.' % y)
+#
+#
+# hilarious = False
+# joke_evaluation = 'Isn\'t that joke so funny?! %r'
+#
+# print(joke_evaluation % hilarious)
+#
+#
+# w = 'This is the left side of ...'
+# e = "a string with a right side."
+# print(w + e)
+#
+# print('Mary had a little lamb.')
+# print('Its fleece was white as %s.' % 'snow')
+# print('And everwhere that Mary went.')
+# print('.' * 10)  # what'd that do
+#
+# end1 = 'C'
+# end2 = 'h'
+# end3 = 'e'
+# end4 = 'e'
+# end5 = 's'
+# end6 = 'e'
+# end7 = 'B'
+# end8 = 'u'
+# end9 = 'r'
+# end10 = 'g'
+# end11 = 'e'
+# end12 = 'r'
+# print(end1 + end2 + end3 + end4 + end5 + end6,
+#       end7 + end8 + end9 + end10 + end11 + end12)
+# print(end7 + end8 + end9 + end10 + end11 + end12)
+#
+#
+# formatter = '%r %r %r %r'
+# print(formatter % (1, 2, 3, 4))
+# print(formatter % ('one', 'two', 'three', 'four'))
+# print(formatter % (True, False, False, True))
+# print(formatter % (formatter, formatter, formatter, formatter))
+# print(formatter % (
+#     'I had this thing.',
+#     "That you could type up right.",
+#     r"But it din't sing.",
+#     "So I said goodnight."
+# ))
+#
+#
+# days = 'Mon Tue Wed Thu Fri Sat Sun'
+# months = '\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
+# print('Here are the days:', days)
+# print('Here are the months:', months)
+#
+# print('''
+#  There's something going on here.
+#  With the three double-quotes.
+#  We'll be able to type as much as we like.
+#  Even 4 lines if we want, or 5, or6.
+#  ''')
+#
+#
+# tabby_cat='\tI\'m tabbed in.'
+# persian_cat="I'm split\non a line."
+# backslash_cat="I'm \\ a \\ cat."
+# fat_cat='''
+# I'll do a list:
+# \t* Cat Food
+# \t* Fishies
+# \t* Catnip\n\t* Grass
+# '''
+# print(tabby_cat)
+# print(persian_cat)
+# print(backslash_cat)
+# print(fat_cat)
+
+
+# while True:
+#     for i in ["/", "-", "丨", "\\", "丨"]:
+#         print("%s\r" % i)
+
+
